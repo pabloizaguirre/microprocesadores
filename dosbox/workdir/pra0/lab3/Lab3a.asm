97fd844b70248df1b5d0baa3bc16e334bd9e12fb
pabloizaguirre <pablo@izaguirre.eu>
2021-04-22 11:31:55 +0200

    Avanzado con el task 2

--- dosbox/workdir/pra0/lab3/Lab3a.asm
+++ dosbox/workdir/pra0/lab3/Lab3a.asm
@@ -24,6 +24,11 @@ _TEXT SEGMENT BYTE PUBLIC 'CODE'
 		
 	_MAIN ENDP 
 
+
+	;******************
+	; DRAW PIXEL 1 F1
+	;******************
+
 	_drawPixel proc far
 		PUSH BP
 		MOV BP, SP
@@ -89,7 +94,7 @@ _TEXT SEGMENT BYTE PUBLIC 'CODE'
 	_drawPixel endp
 
 	;******************
-	; DRAW SQUARE 3A F2
+	; DRAW SQUARE 1 F2
 	;******************
 
 	_drawSquare proc far
@@ -201,7 +206,7 @@ _TEXT SEGMENT BYTE PUBLIC 'CODE'
 
 		mov ah, 00h ; Restore the input configuration to video mode
 		mov al, MODO_VIDEO ; 
-		int 10h∑
+		int 10h
 
 		MOV AX, 0
 		POP SI
@@ -214,7 +219,92 @@ _TEXT SEGMENT BYTE PUBLIC 'CODE'
 		RET
 	_drawSquare endp
 
+	;***********************
+	; DRAW PIXELS LIST 2 F3
+	;***********************
+
 	_drawPixelsList proc far
+		push bp
+		mov bp, sp
+		push ds
+		push es
+		push sp
+		push bx
+		push di
+		push si
+		push cx
+		push dx
+
+		; We use 10h interrupción to enter in video mode
+		MOV AH,0Fh ; Asking for video mode
+		INT 10h ; Call to BIOS
+		MOV MODO_VIDEO,AL ; We save the video mode and store it into AL
+
+		mov ah, 00h ; We set the video mode
+		mov al, 12h ; 640x480 16 color graphics (VGA)
+		int 10h
+
+		mov bl, byte ptr [bp + 8]
+		mov ah, 00h
+		; We set the background colour to the color stored in bx
+		mov ah, 0bh
+		int 10h
+		
+		mov ds, [bp + 15]	; ds<-segment(pixellistx)
+		
+		;mov es, [bp + 19]	; es<-segment(pixellisty)
+		;mov di, [bp + 17]	; di<-offset(pixellisty)
+
+		;mov sp, [bp + 23]	; sp<-segment(pixellistColor)
+
+		mov si, 0
+		mov di, 0 
+
+	draw_pixels_list_ir:
+		mov bx, [bp + 13]	; bx<-offset(pixelistx)
+		mov cx, ds:[bx + di]
+		
+		mov es, [bp + 19]	; es<-segment(pixellisty)
+		mov bx, [bp + 17]	; bx<-offset(pixellisty)
+		mov dx, es:[bx + di]
+		
+		mov es, [bp + 23]	; es<-segment(pixellistColor)
+		mov bx, [bp + 21]	; ax<-segment(pixellistColor)
+		mov ax, es:[bx + si]
+
+		;Int10H draw pixel --> AH=0Ch 	AL = Colour, BH = PageNumber, CX = x, DX = y
+		mov ah, 0Ch
+		; Read from the stack the value for the colour
+		mov al, bl ; white colour 1111b
+		mov bh, 00h ; page number (keep it always to zero)
+		int 10h
+
+		inc si
+		add di, 2
+		cmp si, [bp + 6]
+		jl draw_pixels_list_ir
+
+		mov cx, [bp + 11]
+		mov dx, [bp + 9]
+
+		;Int15H active waiting in milliseconds: 1 millon us = 1 segundo
+		MOV     AH, 86H ;int15h with AH=86h to microseconds waiting in CX:DX
+		INT     15H
+
+		mov ah, 00h ; Restore the input configuration to video mode
+		mov al, MODO_VIDEO ; 
+		int 10h
+
+		mov ax, 0
+		pop dx
+		pop cx
+		pop si
+		pop di
+		pop bx
+		pop sp
+		pop es
+		pop ds
+		pop bp
 		ret
 	_drawPixelsList endp
 
